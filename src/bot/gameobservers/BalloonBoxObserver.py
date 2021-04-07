cc21c7b5f3356d4d6dbd32a9b3d8147589d8b3d0
Steven <4smalmgren@gmail.com>
2021-04-06 17:08:15 -0700

    better time

--- src/bot/gameobservers/BalloonBoxObserver.py
+++ src/bot/gameobservers/BalloonBoxObserver.py
@@ -12,10 +12,10 @@ class BalloonBoxTeamObserver(Observer):
         if subject.won and subject.team_data.num_teams == 2:
             if 0 in subject.winning_team_ids or subject.winning_team_id == 0:
                 logging.debug("Team 0 win. Inflating left balloon.")
-                self.balloon_box.left_pump(2000)
+                self.balloon_box.left_pump(3500)
             if 1 in subject.winning_team_ids or subject.winning_team_id == 1:
                 logging.debug("Team 1 win. Inflating right balloon.")
-                self.balloon_box.right_pump(2000)
+                self.balloon_box.right_pump(3500)
 
 
 
